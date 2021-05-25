a77af1bb5b55a06f2049d244aee67f65305a4527
Layuntafelipe <flobo0701@icloud.com>
2021-05-25 15:10:07 -0300

    Initial Commit

---
+++ SimpleCards/SimpleCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCardsApp.swift
+//  SimpleCards
+//
+//  Created by Felipe Lobo on 25/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
